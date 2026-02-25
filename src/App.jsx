ea4b6628829cc8fef103dc9abafe1cf657a69a77
Aleksandr <ichmarchuk@gail.com>
2026-02-25 11:47:27 +0100

    add Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
 import "./App.css";
 
+import Header from "./components/header";
+// import PostList from "./components/postList";
+// import PostForm from "./components/postForm";
+
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 }
 
 export default App;
--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,1 +1,22 @@
 import styles from "./styles.module.css";
+import React from "react";
+import logo from "../../assets/images/logo.png";
+
+const Header = () => {
+  return (
+    <header className={styles.header}>
+      <div>
+        <a href="/">
+          <img src={logo} alt="Cooljob" />
+        </a>
+      </div>
+      <nav className={styles.nav}>
+        <a href="/link">Главная</a>
+        <a href="/link">Музыка</a>
+        <a href="/link">Сообщества</a>
+        <a href="/link">Друзья</a>
+      </nav>
+    </header>
+  );
+};
+export default Header;
