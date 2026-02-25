b930a94799187d1f3dd11c8436883aafefc2fa1c
Aleksandr <ichmarchuk@gail.com>
2026-02-25 13:51:20 +0100

    full version  PostForm

--- src/components/postForm/index.jsx
+++ src/components/postForm/index.jsx
@@ -6,31 +6,52 @@ import { useForm } from "react-hook-form";
 const PostForm = () => {
   const { register, handleSubmit, reset } = useForm();
 
+  const onSubmit = (data) => {
+    const postData = {
+      title: data.title,
+      text: data.content,
+    };
+
+    axios
+      .post("https://699eb2fd78dda56d396b07c2.mockapi.io/posts", postData)
+      .then((response) => {
+        console.log("Данные отправлены", response);
+        reset();
+      })
+      .catch((error) => {
+        console.error("Ошибка отправки", error);
+      });
+  };
+
   return (
-    <section className="styles.section">
-      <h1>Написать пост</h1>
-      <form onSubmit={handleSubmit(onSubmit)} className="styles.form">
-        <div className="styles.formGroup">
+    <section className={styles.section}>
+      <h1 className={styles.title}>Написать пост</h1>
+      <form onSubmit={handleSubmit(onSubmit)} className={styles.form}>
+        <div className={styles.formGroup}>
           <label htmlFor="title">Заголовок</label>
           <input
             type="text"
             id="title"
-            {...register("title", { reguired: true })}
+            {...register("title", { required: true })}
+            className={styles.input}
+            placeholder="Заголовок"
           />
         </div>
-        <div className="styles.formGroup">
-          <label htmlFor="content">Контент</label>
-          <input
-            type="text"
-            id="title"
-            {...register("title", { reguired: true })}
+        <div className={styles.formGroup}>
+          <label htmlFor="content">Текст поста</label>
+          <textarea
+            id="content"
+            {...register("content", { required: true })}
+            className={styles.textarea}
+            placeholder="Введите текст"
           />
         </div>
-        <button type="submit" className="styles.button">
-          Отправить
+        <button type="submit" className={styles.button}>
+          Публикация
         </button>
       </form>
     </section>
   );
 };
+
 export default PostForm;
